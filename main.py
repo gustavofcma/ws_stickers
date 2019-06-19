ea611e7b768225078974e7671d66d120f23e661f
Gustavo Figueiredo Costa Monteiro de Almeida - DATAPREVRJ <gustavo.dealmeida@dataprev.gov.br>
2019-06-19 17:28:27 -0300

    add: json example

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
+import io
+import json
+import base64
 from PIL import Image, ImageDraw, ImageFont
+# from io import BytesIO
 
 BASE_DIR = '.'
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -13,25 +17,49 @@ STICKERS_DIR = os.path.join(BASE_DIR, 'static')
 # TST_DIR = './tst'
 TST_DIR = os.path.join(BASE_DIR, 'tst')
 
+SUPPORTED_FILES = ['png']
 
-fnt = ImageFont.truetype(os.path.join(SRC_DIR, 'comic.ttf'), 130)
+AUTHOR = 'Guga'
+PACK_NAME = 'By @FlorkOfCows'
 
-num = '001'
-fim = '028 A'
+def get_supported_files(directory):
+    only_files = [item for item in os.listdir(directory) if os.path.isfile(os.path.join(directory, item))]
+    return [file_item for file_item in only_files if file_item.split('.')[1].lower() in SUPPORTED_FILES]
 
-base = Image.open(os.path.join(SRC_DIR, 'base_pokeball.png')).convert('RGBA')
-base_w, base_h = base.size
+def main():
+    fnt = ImageFont.truetype(os.path.join(SRC_DIR, 'comic.ttf'), 130)
+    base = Image.open(os.path.join(SRC_DIR, 'base_pokeball.png')).convert('RGBA')
+    base_w, base_h = base.size
+    txt = Image.new('RGBA', base.size, (255,255,255,0))
+    d = ImageDraw.Draw(txt)
+    
+    num = '0032'
+    fim = '028_A'.replace('_',' ')
+        
+    txti_w, txti_h = d.textsize(num, font=fnt)
+    txtf_w, txtf_h = d.textsize(fim, font=fnt)
+    
+    d.text(( (base_w - txti_w)/2 , 10 ), num, font=fnt, fill='white')
+    d.text(( (base_w - txtf_w)/2 , base_h - txtf_h - 60 ), fim, font=fnt, fill='black')
+    
+    out = Image.alpha_composite(base, txt)
+    
+    out.show()
+    # out.save(os.path.join(BASE_DIR, 'teste.png'), 'PNG')
 
-txt = Image.new('RGBA', base.size, (255,255,255,0))
-d = ImageDraw.Draw(txt)
+'''
+    with open("yourfile.ext", "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
 
-txti_w, txti_h = d.textsize(num, font=fnt)
-txtf_w, txtf_h = d.textsize(fim, font=fnt)
+    buffered = io.BytesIO()
+    out.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue())
+    del buffered
+    print(img_str)
 
-d.text(( (base_w - txti_w)/2 , 10 ), num, font=fnt, fill='white')
-d.text(( (base_w - txtf_w)/2 , base_h - txtf_h - 60 ), fim, font=fnt, fill='black')
+    with open('data.json', 'w') as fp:
+    json.dump(data, fp)
+'''
 
-out = Image.alpha_composite(base, txt)
-
-out.show()
-# out.save(os.path.join(BASE_DIR, 'teste.png'), 'PNG')
+if __name__ == '__main__':
+    main()
